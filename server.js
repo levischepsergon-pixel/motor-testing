1abf4e63e51af0d1fb1bf7653af63f6e764bde5e
levischepsergon-pixel <levischepsergon@gmail.com>
2026-05-08 01:20:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -46,4 +46,4 @@ app.post("/sensors", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Limika Cloud Engine Solid on port ${PORT}`));.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Limika Cloud Engine Solid on port ${PORT}`));
